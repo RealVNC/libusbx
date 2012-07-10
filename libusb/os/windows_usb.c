be904c4121476129ba5ddc42bb7b8ececdf98da3
Sorin Basca <sb2@realvnc.com>
2012-07-10 11:06:03 +0100

    Avoiding repeats of HID interfaces on Windows.
    When setting a HID interface, its path would just be added as the next item in the windows_device_priv usb_interface array. If requesting the device list several times within the same context, and when the device gets added and removed repeatedly, the same path was being added. This caused the number of interfaces to go beyond the maximum number of USB interfaces (of 32).
    
    To fix this problem, I added a check that the interface limit has not been reached. If the limit is reached I report an error. Also there is a check that the path being added is not already present in the interfaces array. If found in the array, success is reported and the number of interfaces doesn't increase anymore.

--- libusb/os/windows_usb.c
+++ libusb/os/windows_usb.c
@@ -1203,11 +1203,25 @@ static int set_hid_interface(struct libusb_context* ctx, struct libusb_device* d
 							char* dev_interface_path)
 {
 	struct windows_device_priv *priv = _device_priv(dev);
+	size_t i = 0; 
 
 	if (priv->hid == NULL) {
 		usbi_err(ctx, "program assertion failed: parent is not HID");
 		return LIBUSB_ERROR_NO_DEVICE;
 	}
+	if (priv->hid->nb_interfaces == USB_MAXINTERFACES) {
+		usbi_err(ctx, "program assertion failed: max USB interfaces reached for HID device");
+		return LIBUSB_ERROR_NO_DEVICE;
+	}
+
+	for(i = 0; i < priv->hid->nb_interfaces; i++) {
+		if((priv->usb_interface[i].apib == &usb_api_backend[USB_API_HID]) &&
+				(safe_strcmp(priv->usb_interface[i].path, dev_interface_path) == 0)) {
+			// Avoid adding the same interface path twice
+			return LIBUSB_SUCCESS;
+		}
+	}
+
 	priv->usb_interface[priv->hid->nb_interfaces].path = dev_interface_path;
 	priv->usb_interface[priv->hid->nb_interfaces].apib = &usb_api_backend[USB_API_HID];
 	usbi_dbg("interface[%d] = %s", priv->hid->nb_interfaces, dev_interface_path);
